664987cc0841e58b914b4c22ceb0be3bf6cd585a
Jeremontoya155 <jeremontoya155@gmail.com>
2026-04-27 00:23:12 -0300

    config nuevo

--- config/db.js
+++ config/db.js
@@ -1,8 +1,22 @@
 const { Pool } = require('pg');
 
+function shouldUseSsl(connectionString) {
+  if (!connectionString) {
+    return false;
+  }
+
+  try {
+    const url = new URL(connectionString);
+    const sslMode = url.searchParams.get('sslmode');
+    return sslMode !== 'disable';
+  } catch (_error) {
+    return true;
+  }
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: shouldUseSsl(process.env.DATABASE_URL) ? { rejectUnauthorized: false } : false
 });
 
 pool.on('error', (err) => {
